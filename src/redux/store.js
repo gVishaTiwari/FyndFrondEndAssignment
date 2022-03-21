28b6f41f39cc668254b9a3f599cdc842e297f97f
Vishal Tiwari <gvishaltiwari@gmail.com>
2022-03-21 14:13:57 +0530

    store read only

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,9 +6,9 @@ import rootReducer from "./rootReducer";
 const initialState = {};
 const middleware = [thunk];
 
-export const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-// export const store = createStore(rootReducer, applyMiddleware(thunk));
+// export const store = createStore(rootReducer, initialState, compose(
+//     applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+export const store = createStore(rootReducer, applyMiddleware(thunk));
 if(process.env.NODE_ENV === 'production') {
     store = createStore(rootReducer, initialState, compose(
         applyMiddleware(...middleware)
