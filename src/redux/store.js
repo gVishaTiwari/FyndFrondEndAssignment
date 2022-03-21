da941d039763e19346706bcc391d9647a64b64d3
Vishal Tiwari <gvishaltiwari@gmail.com>
2022-03-21 14:18:26 +0530

    store read only removed

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,18 +6,18 @@ import rootReducer from "./rootReducer";
 const initialState = {};
 const middleware = [thunk];
 
-// export const store = createStore(rootReducer, initialState, compose(
-//     applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-export const store = createStore(rootReducer, applyMiddleware(thunk));
-if(process.env.NODE_ENV === 'production') {
-    store = createStore(rootReducer, initialState, compose(
-        applyMiddleware(...middleware)
-    ));
-} else {
-    store = createStore(rootReducer, initialState, compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ));
-}
+export const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+// export const store = createStore(rootReducer, applyMiddleware(thunk));
+// if(process.env.NODE_ENV === 'production') {
+//     store = createStore(rootReducer, initialState, compose(
+//         applyMiddleware(...middleware)
+//     ));
+// } else {
+//     store = createStore(rootReducer, initialState, compose(
+//         applyMiddleware(...middleware),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     ));
+// }
 
-export default { store, };
+// export default { store, };
